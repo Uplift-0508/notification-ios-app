c617c509aa76d806dc0a25611fc419c62ce587b1
yikyung <uplift.0508@gmail.com>
2022-01-12 15:56:58 -0900

    initialize

---
+++ notification-ios-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  notification-ios-app
+//
+//  Created by yikyung on 12/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ notification-ios-app/notification_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  notification_ios_appApp.swift
+//  notification-ios-app
+//
+//  Created by yikyung on 12/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct notification_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
